8b01972f385edb25496a4dc9bb3d0b7c48267f82
John Calvin <ashannon1990@gmail.com>
2019-04-01 15:25:57 -0400

    buttons created, add buttons working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,1 +1,28 @@
 var sportsArray = ["Football", "Soccer", "Baseball", "Gymnastics", "Jiu Jitsu", "Mixed Martial Arts"]
+
+function createButtons() {
+    $("#sportsButtons").empty();
+    for (var i = 0; i < sportsArray.length; i++) {
+        var a = $("<button>");
+        a.addClass("sports-btn btn btn-dark");
+        a.attr("data-name", sportsArray[i]);
+        a.text(sportsArray[i]);
+        console.log(sportsArray[i])
+        $("#sportsButtons").append(a);
+    }
+}
+$("#addBtn").on("click", function(event) {
+    event.preventDefault();
+    var sports = $(".addSport").val().trim();
+    sportsArray.push(sports);
+    createButtons();
+})
+createButtons();
+
+var sportSelect = $(".btn").text()
+
+
+$(".sports-btn").click(function(){
+    console.log(sportSelect)
+})
+
